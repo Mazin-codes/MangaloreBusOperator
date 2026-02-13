1e39789d5f604749f0779fb7bc4b5cf36d56f529
adithyan <adithyan.m.r.no12@gmail.com>
2026-02-13 18:10:47 +0530

    test

--- app/src/main/java/com/example/bustrackoperator/MainActivity.kt
+++ app/src/main/java/com/example/bustrackoperator/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.bustrackoperator.ui.DriverScreen
+import com.example.bustrackoperator.ui.theme.DriverScreen
 import com.example.bustrackoperator.ui.theme.BusTrackOperatorTheme
 
 class MainActivity : ComponentActivity() {
